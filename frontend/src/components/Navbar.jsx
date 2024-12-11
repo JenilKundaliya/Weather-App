c663493e1f9db6ce817ba0c591f737b275751f80
JenilKundaliya <jenil.kundaliya@gmail.com>
2024-12-11 18:33:02 +0530

    responsiveness

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 const Navbar = () => {
   return (
     <div className="navbar col-12 w-100 row shadow">
-      <div className="col-8 row">
-        <div className="col-4">
-          <h3>Weather App</h3>
+      <div className="col-md-8 col-12 row">
+        <div className="col-8 col-md-4">
+          <h3 className="text-wrap">Weather App</h3>
         </div>
         <div className="col-8"></div>
       </div>
--- frontend/src/components/WeatherApp.jsx
+++ frontend/src/components/WeatherApp.jsx
@@ -9,6 +9,7 @@ const WeatherApp = () => {
   const [weatherData, setWeatherData] = useState({});
   const [error, setError] = useState("");
   const [isCheckValid, setIsCheckValid] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const getWeatherIcon = (iconCode) => {
     const iconMap = {
       "01d": <Sun size={64} className="bg-white text-yellow-500" />,
@@ -28,6 +29,7 @@ const WeatherApp = () => {
   };
 
   const fetchWeatherData = async () => {
+    setIsLoading(true);
     setIsCheckValid(true);
     if (!city) {
       setError("Please enter a city name");
@@ -41,11 +43,12 @@ const WeatherApp = () => {
       if (!response?.data || response?.data?.code !== 200) {
         setError(response?.data?.error ?? "Something Went Wrong");
       }
-
+      setIsLoading(false);
       setWeatherData(response?.data?.data);
       setCity("");
       setError("");
     } catch (err) {
+      setIsLoading(false);
       console.log("errerr", err);
       setError(err?.response?.data?.error ?? "Something Went Wrong");
       setWeatherData(null);
@@ -97,9 +100,9 @@ const WeatherApp = () => {
   ];
   return (
     <div className="px-4 py-2 weather-app-main col-12 d-flex flex-column align-items-center">
-      <div className="city-search d-flex flex-column my-4 col-8">
+      <div className="city-search d-flex flex-column my-4 col-12 col-md-8">
         <h3 className="text-light">Check Weather For Your City</h3>
-        <div className="col-6 d-flex flex-row">
+        <div className="col-12 col-md-6 d-flex flex-column flex-md-row">
           <input
             type="text"
             className="city-search-input form-control rounded-0"
@@ -116,7 +119,7 @@ const WeatherApp = () => {
           />
 
           <button
-            className="btn btn-outline-success rounded-0 ms-2 px-2 text-nowrap bg-light text-dark border-light"
+            className="btn btn-outline-success rounded-0 ms-0 ms-md-2 mt-md-0 mt-2 px-2 text-nowrap bg-light text-dark border-light"
             onClick={() => {
               fetchWeatherData();
             }}
@@ -133,8 +136,8 @@ const WeatherApp = () => {
       )} */}
       {weatherData && Object.keys(weatherData)?.length > 0 ? (
         <div className="col-12 d-flex flex-column align-items-center mt-4">
-          <div className="col-8 d-flex flex-row justify-content-center align-items-center p-2 border border-light shadow py-4">
-            <div className="city col-6">
+          <div className="col-12 col-md-8 d-flex flex-column flex-sm-row justify-content-center align-items-center p-2 border border-light shadow py-4">
+            <div className="city col-12 col-sm-6 d-flex flex-column align-items-center align-items-sm-start">
               <div className="title text-light">
                 <h2>
                   {weatherData.name}, {weatherData.sys.country}
@@ -150,11 +153,11 @@ const WeatherApp = () => {
                 </h3>
               </div>
             </div>
-            <div className="col-6 d-flex justify-content-end px-3">
+            <div className="col-12 col-sm-6 d-flex justify-content-sm-end justify-content-center px-3">
               {getWeatherIcon(weatherData?.weather[0]?.icon)}
             </div>
           </div>
-          <div className="col-8 d-flex mt-4 text-start">
+          <div className="col-12 col-md-8 d-flex mt-4 text-start">
             <p
               className="text-start text-light fw-bold"
               style={{ fontSize: "20px" }}
@@ -162,10 +165,10 @@ const WeatherApp = () => {
               Weather Parameters
             </p>
           </div>
-          <div className="col-8 row border border-light shadow-lg p-2">
+          <div className="col-12 col-md-8 row border border-light shadow-lg p-2">
             {weatherParameters?.map((val, idx) => {
               return (
-                <div className="weather-parameter col-4 p-2" key={idx}>
+                <div className="weather-parameter col-6 col-md-4 p-2" key={idx}>
                   <div className="weather-parameter-child rounded w-100 d-flex flex-column align-items-center justify-content-center">
                     <p className="wp-title fs-5">{val?.title}</p>
                     <p className="wp-desc fs-4">
@@ -179,11 +182,12 @@ const WeatherApp = () => {
         </div>
       ) : (
         <div className="col-12 d-flex flex-column no-weather-data justify-content-center align-items-center mt-4">
-          <div className="col-8 d-flex flex-column justify-content-center align-items-center border border-light py-4">
+          <div className="col-12 col-md-8 d-flex flex-column justify-content-center align-items-center border border-light py-4">
             <img
               src="/assets/no-data.png"
               style={{ width: "60px", height: "60px", marginBottom: "12px" }}
             />
+
             <h3 className="text-light mb-0 fs-2">No Data</h3>
             <p className="text-light">
               {error ?? "Enter City Name to check Weather."}
